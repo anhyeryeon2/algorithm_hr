127f97f45bc0c19ac0443120c0a8da9aef247322
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2024-04-22 23:12:13 +0900

    [Silver IV] Title: 카드2, Time: 200 ms, Memory: 50964 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,15 +1,13 @@
-# 카드2
 from collections import deque
 
 N = int(input())
-# dp =deque()
-# for i in range(1,N + 1):
-#     dp.append(i)
 
-dp =deque(range(1,N + 1))
+dp =deque()
+for i in range(1,N + 1):
+    dp.append(i)
 
 while len(dp)>1:
     dp.popleft()
     dp.append(dp.popleft())
 
-print(dp.popleft())
+print(dp[0])
