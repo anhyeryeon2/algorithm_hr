7a0b600612ea0a6a53790b64a6ea836dce57aafe
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2024-08-26 19:10:44 +0900

    [Silver III] Title: N과 M (2), Time: 92 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.js
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.js
@@ -4,27 +4,21 @@ let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 let [n, m] = input[0].split(" ").map(Number);
 let arr = [];
 for (let i = 1; i <= n; i++) arr.push(i);
-let visited = new Array(n).fill(false);
 let selected = [];
-
 let answer = "";
+
 function dfs(arr, depth, start) {
-  if(depth == m){
-  let result = [];
-  for (let i of selected) result.push(arr[i]);
-  for (let x of result) answer += x + " ";
-  answer += "\n";
-  return;
+  if (depth == m) {
+    answer += selected.join(" ") + "\n";
+    return;
+  }
+
+  for (let i = start; i < arr.length; i++) {
+    selected.push(arr[i]);
+    dfs(arr, depth + 1, i + 1);
+    selected.pop();
+  }
 }
 
-for (let i = start; i < arr.length; i++) {
-  if (visited[i]) continue;
-  selected.push(i);
-  visited[i] = true;
-  dfs(arr, depth + 1, i + 1);
-  selected.pop();
-  visited[i] = false;
-}
-}
 dfs(arr, 0, 0);
-console.log(answer);
+console.log(answer.trim());
